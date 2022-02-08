83971d417d825fcd56b230def3141f1e09463a7f
knolan18 <minicreature@Kellys-Mac-mini.local>
2022-02-07 18:02:20 -0800

    changed routes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -4,12 +4,6 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init({
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
     product_id: {
       type: DataTypes.INTEGER,
       references: {
